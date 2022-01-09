e3cdb717e9bc840c2518130852362435ab1e55a4
mukesh <mukeshjha2107@gmail.com>
2022-01-09 15:04:13 +0530

    mock file added

---
+++ mock.js
@@ -0,0 +1,565 @@
+
+const dumyName=[
+  {
+    "name": "Ajay",
+    "username": "Ajay",
+    "address": {
+      "streetA": "Madhubani",
+      "streetB": "Madhubani",
+      "streetC": "Madhubani",
+      "streetD": "Madhubani",
+      "city": " madhubani",
+      "state": "Bihar",
+      "country": "India",
+      "zipcode": "847224",
+      "geo": {
+        "lat": "77.77",
+        "lng": "77.77"
+      }
+    },
+    "website": "",
+    "bio":
+      " ",
+    "company": {
+      "name": " ",
+      "catchPhrase": " ",
+      "bs": " "
+    },
+    "avatar": "",
+    "avatarBackground":
+      " ",
+    "tels": [
+      { "id": 1, "name": "Mobile", "number": "" },
+      { "id": 2, "name": "Work", "number": "" }
+    ],
+    "emails": [
+      { "id": 1, "name": "Personal", "email": "badcodercpp@bjshub.com" },
+      { "id": 2, "name": "Work", "email": "badcodercpp@bjshub.com " }
+    ],
+    "posts": [
+      {
+        "id": 1,
+        "words": " ",
+        "sentence": " ",
+        "sentences":
+          " ",
+        "paragraph":"",
+            
+        
+        
+        "user": {
+          "name": " ",
+          "username": " ",
+          "avatar":
+            " ",
+          "email": " "
+        }
+      },
+      {
+        "id": 2,
+        "words": " ",
+        "sentence": " ",
+        "sentences":
+          " ",
+        "paragraph":
+          " ",
+        "createdDate": " ",
+        "user": {
+          "name": " ",
+          "username": " ",
+          "avatar":
+            " ",
+          "email": " "
+        }
+      },
+      {
+        "id": 3,
+        "words": " ",
+        "sentence": " ",
+        "sentences":
+          "  ",
+        "paragraph":
+          " ",
+        "createdDate": " ",
+        "image": " ",
+        "user": {
+          "name": " ",
+          "username": " ",
+          "avatar":
+            " ",
+          "email": " "
+        }
+      }
+    ]
+  },
+
+  {
+    "name": "Manish",
+    "username": "Manish",
+    "address": {
+      "streetA": "Hajipur",
+      "streetB": "Hajipur",
+      "streetC": "Hajipur",
+      "streetD": "Hajipur",
+      "city": " Hajipur",
+      "state": "Bihar",
+      "country": "India",
+      "zipcode": "847224",
+      "geo": {
+        "lat": "77.77",
+        "lng": "77.77"
+      }
+    },
+    "website": "",
+    "bio":
+      " ",
+    "company": {
+      "name": " ",
+      "catchPhrase": " ",
+      "bs": " "
+    },
+    "avatar": "",
+    "avatarBackground":
+      " ",
+    "tels": [
+      { "id": 1, "name": "Mobile", "number": "" },
+      { "id": 2, "name": "Work", "number": "" }
+    ],
+    "emails": [
+      { "id": 1, "name": "Personal", "email": "manishkr@gmail.com" },
+      { "id": 2, "name": "Work", "email": "manishkr@gmail.com " }
+    ],
+    "posts": [
+      {
+        "id": 1,
+        "words": " ",
+        "sentence": " ",
+        "sentences":
+          " ",
+        "paragraph":"",
+            
+        
+        
+        "user": {
+          "name": " ",
+          "username": " ",
+          "avatar":
+            " ",
+          "email": " "
+        }
+      },
+      {
+        "id": 2,
+        "words": " ",
+        "sentence": " ",
+        "sentences":
+          " ",
+        "paragraph":
+          " ",
+        "createdDate": " ",
+        "user": {
+          "name": " ",
+          "username": " ",
+          "avatar":
+            " ",
+          "email": " "
+        }
+      },
+      {
+        "id": 3,
+        "words": " ",
+        "sentence": " ",
+        "sentences":
+          "  ",
+        "paragraph":
+          " ",
+        "createdDate": " ",
+        "image": " ",
+        "user": {
+          "name": " ",
+          "username": " ",
+          "avatar":
+            " ",
+          "email": " "
+        }
+      }
+    ]
+  },
+
+  {
+    "name": "Manmohan",
+    "username": "Manmohan",
+    "address": {
+      "streetA": "Delhi",
+      "streetB": "Delhi",
+      "streetC": "Delhi",
+      "streetD": "Delhi",
+      "city": " Delhi",
+      "state": "Delhi",
+      "country": "India",
+      "zipcode": "110001",
+      "geo": {
+        "lat": "77.77",
+        "lng": "77.77"
+      }
+    },
+    "website": "",
+    "bio":
+      " ",
+    "company": {
+      "name": " ",
+      "catchPhrase": " ",
+      "bs": " "
+    },
+    "avatar": "",
+    "avatarBackground":
+      " ",
+    "tels": [
+      { "id": 1, "name": "Mobile", "number": "" },
+      { "id": 2, "name": "Work", "number": "" }
+    ],
+    "emails": [
+      { "id": 1, "name": "Personal", "email": "manmohanprakash@gmail.com" },
+      { "id": 2, "name": "Work", "email": "manmohanprakash@gmail.com" }
+    ],
+    "posts": [
+      {
+        "id": 1,
+        "words": " ",
+        "sentence": " ",
+        "sentences":
+          " ",
+        "paragraph":"",
+            
+        
+        
+        "user": {
+          "name": " ",
+          "username": " ",
+          "avatar":
+            " ",
+          "email": " "
+        }
+      },
+      {
+        "id": 2,
+        "words": " ",
+        "sentence": " ",
+        "sentences":
+          " ",
+        "paragraph":
+          " ",
+        "createdDate": " ",
+        "user": {
+          "name": " ",
+          "username": " ",
+          "avatar":
+            " ",
+          "email": " "
+        }
+      },
+      {
+        "id": 3,
+        "words": " ",
+        "sentence": " ",
+        "sentences":
+          "  ",
+        "paragraph":
+          " ",
+        "createdDate": " ",
+        "image": " ",
+        "user": {
+          "name": " ",
+          "username": " ",
+          "avatar":
+            " ",
+          "email": " "
+        }
+      }
+    ]
+  },
+
+  {
+    "name": "Sauvik",
+    "username": "Sauvik",
+    "address": {
+      "streetA": "kolkata",
+      "streetB": "kolkata",
+      "streetC": "kolkata",
+      "streetD": "kolkata",
+      "city": " kolkata",
+      "state": "WestBengal",
+      "country": "India",
+      "zipcode": "700001",
+      "geo": {
+        "lat": "77.77",
+        "lng": "77.77"
+      }
+    },
+    "website": "",
+    "bio":
+      " ",
+    "company": {
+      "name": " ",
+      "catchPhrase": " ",
+      "bs": " "
+    },
+    "avatar": "",
+    "avatarBackground":
+      " ",
+    "tels": [
+      { "id": 1, "name": "Mobile", "number": "" },
+      { "id": 2, "name": "Work", "number": "" }
+    ],
+    "emails": [
+      { "id": 1, "name": "Personal", "email": "sauvikghosh@gmail.com" },
+      { "id": 2, "name": "Work", "email": "sauvikghosh@gmail.com" }
+    ],
+    "posts": [
+      {
+        "id": 1,
+        "words": " ",
+        "sentence": " ",
+        "sentences":
+          " ",
+        "paragraph":"",
+            
+        
+        
+        "user": {
+          "name": " ",
+          "username": " ",
+          "avatar":
+            " ",
+          "email": " "
+        }
+      },
+      {
+        "id": 2,
+        "words": " ",
+        "sentence": " ",
+        "sentences":
+          " ",
+        "paragraph":
+          " ",
+        "createdDate": " ",
+        "user": {
+          "name": " ",
+          "username": " ",
+          "avatar":
+            " ",
+          "email": " "
+        }
+      },
+      {
+        "id": 3,
+        "words": " ",
+        "sentence": " ",
+        "sentences":
+          "  ",
+        "paragraph":
+          " ",
+        "createdDate": " ",
+        "image": " ",
+        "user": {
+          "name": " ",
+          "username": " ",
+          "avatar":
+            " ",
+          "email": " "
+        }
+      }
+    ]
+  },
+
+
+  {
+    "name": "Mayank",
+    "username": "Mayank",
+    "address": {
+      "streetA": "Pratapganj",
+      "streetB": "Pratapganj",
+      "streetC": "Pratapganj",
+      "streetD": "Pratapganj",
+      "city": " Supaul",
+      "state": "Bihar",
+      "country": "India",
+      "zipcode": "852125",
+      "geo": {
+        "lat": "77.77",
+        "lng": "77.77"
+      }
+    },
+    "website": "",
+    "bio":
+      " ",
+    "company": {
+      "name": " ",
+      "catchPhrase": " ",
+      "bs": " "
+    },
+    "avatar": "",
+    "avatarBackground":
+      " ",
+    "tels": [
+      { "id": 1, "name": "Mobile", "number": "7766065881" },
+      { "id": 2, "name": "Work", "number": "7766065881" }
+    ],
+    "emails": [
+      { "id": 1, "name": "Personal", "email": "mstict16@gmail.com" },
+      { "id": 2, "name": "Work", "email": "mstict16@gmail.com" }
+    ],
+    "posts": [
+      {
+        "id": 1,
+        "words": " ",
+        "sentence": " ",
+        "sentences":
+          " ",
+        "paragraph":"",
+            
+        
+        
+        "user": {
+          "name": " ",
+          "username": " ",
+          "avatar":
+            " ",
+          "email": " "
+        }
+      },
+      {
+        "id": 2,
+        "words": " ",
+        "sentence": " ",
+        "sentences":
+          " ",
+        "paragraph":
+          " ",
+        "createdDate": " ",
+        "user": {
+          "name": " ",
+          "username": " ",
+          "avatar":
+            " ",
+          "email": " "
+        }
+      },
+      {
+        "id": 3,
+        "words": " ",
+        "sentence": " ",
+        "sentences":
+          "  ",
+        "paragraph":
+          " ",
+        "createdDate": " ",
+        "image": " ",
+        "user": {
+          "name": " ",
+          "username": " ",
+          "avatar":
+            " ",
+          "email": " "
+        }
+      }
+    ]
+  },
+
+  {
+    "name": "Ms",
+    "username": "Ms",
+    "address": {
+      "streetA": "Pratapganj",
+      "streetB": "Pratapganj",
+      "streetC": "Pratapganj",
+      "streetD": "Pratapganj",
+      "city": " Supaul",
+      "state": "Bihar",
+      "country": "India",
+      "zipcode": "852125",
+      "geo": {
+        "lat": "77.77",
+        "lng": "77.77"
+      }
+    },
+    "website": "",
+    "bio":
+      " ",
+    "company": {
+      "name": " ",
+      "catchPhrase": " ",
+      "bs": " "
+    },
+    "avatar": "",
+    "avatarBackground":
+      " ",
+    "tels": [
+      { "id": 1, "name": "Mobile", "number": "7766065881" },
+      { "id": 2, "name": "Work", "number": "7766065881" }
+    ],
+    "emails": [
+      { "id": 1, "name": "Personal", "email": "mstict@gmail.com" },
+      { "id": 2, "name": "Work", "email": "mstict@gmail.com" }
+    ],
+    "posts": [
+      {
+        "id": 1,
+        "words": " ",
+        "sentence": " ",
+        "sentences":
+          " ",
+        "paragraph":"",
+            
+        
+        
+        "user": {
+          "name": " ",
+          "username": " ",
+          "avatar":
+            " ",
+          "email": " "
+        }
+      },
+      {
+        "id": 2,
+        "words": " ",
+        "sentence": " ",
+        "sentences":
+          " ",
+        "paragraph":
+          " ",
+        "createdDate": " ",
+        "user": {
+          "name": " ",
+          "username": " ",
+          "avatar":
+            " ",
+          "email": " "
+        }
+      },
+      {
+        "id": 3,
+        "words": " ",
+        "sentence": " ",
+        "sentences":
+          "  ",
+        "paragraph":
+          " ",
+        "createdDate": " ",
+        "image": " ",
+        "user": {
+          "name": " ",
+          "username": " ",
+          "avatar":
+            " ",
+          "email": " "
+        }
+      }
+    ]
+  }
+
+
+
+  
+]
